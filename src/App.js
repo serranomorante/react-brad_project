218e9622d9d44fb5593da70a67eea68797118c7f
serranomorante <serranomorantepatricio@gmail.com>
2019-06-24 22:33:20 -0500

    avances

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
+import User from './components/users/User';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   };
@@ -38,6 +40,19 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  // Get a single Github User
+  getUser = async username => {
+    this.setState({
+      loading: true
+    });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ user: res.data, loading: false });
+  };
+
   // Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
 
@@ -48,7 +63,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, loading, alert } = this.state;
+    const { users, loading, alert, user } = this.state;
     return (
       <Router>
         <div className='App'>
@@ -72,6 +87,18 @@ class App extends Component {
                 )}
               />
               <Route exact path='/about' component={About} />
+              <Route
+                exact
+                path='/user/:login'
+                render={props => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    user={user}
+                    loading={loading}
+                  />
+                )}
+              />
             </Switch>
           </div>
         </div>
---
+++ src/components/users/User.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class User extends Component {
+  componentDidMount() {}
+  render() {
+    return <div>User</div>;
+  }
+}
+
+export default User;
